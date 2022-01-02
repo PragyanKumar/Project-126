c9f3762d0fea4b365f592772459d8e162ee83f69
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2022-01-02 20:09:19 +0530

    Add files via upload

--- Music.js
+++ Music.js
@@ -1,15 +1,36 @@
 song=""
+leftWristX=0
+LeftWristY=0
+RightWristX=0
+RightWristY=0
 
 function preload(){
     song=loadSound("music.mp3")
 }
-
 
 function setup(){
     canvas=createCanvas(600,500)
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
+    posenet=ml5.poseNet(video,modelLoaded)
+    posenet.on("pose" , gotPoses)
+}
+
+function gotPoses(result){
+    if(result.length>0){
+        console.log(result)
+        leftWristX=result[0].pose.leftWrist.x
+        LeftWristY=result[0].pose.leftWrist.y
+        console.log("LeftWristX= " + leftWristX + "LeftWristY= " + LeftWristY)
+        RightWristX=result[0].pose.rightWrist.x
+        RightWristY=result[0].pose.rightWrist.y
+        console.log("RightWristX= " + RightWristX + "RightWristY= " + RightWristY)
+    }
+}
+
+function modelLoaded(){
+    console.log("posenet is initialised")
 }
 
 function draw(){
@@ -18,4 +39,6 @@ function draw(){
 
 function play(){
     song.play()
+    song.setVolume(1)
+    song.rate(1)
 }
