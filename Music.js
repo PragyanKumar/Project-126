20e4cf18a2edce794d73b5e2456e6c2527251e06
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2022-01-15 21:25:19 +0530

    Add files via upload

--- Music.js
+++ Music.js
@@ -1,13 +1,18 @@
-song=""
+song1=""
+song2=""
+song1_status=""
+song2_status=""
 leftWristX=0
 LeftWristY=0
 RightWristX=0
 RightWristY=0
 
 scoreLeftWrist=0
+scoreRightWrist=0
 
 function preload(){
-    song=loadSound("music.mp3")
+    song1=loadSound("music.mp3")
+    song2=loadSound("music2.mp3")
 }
 
 function setup(){
@@ -19,16 +24,17 @@ function setup(){
     posenet.on("pose" , gotPoses)
 }
 
-function gotPoses(result){
-    if(result.length>0){
-        console.log(result)
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results)
         scoreLeftWrist=results[0].pose.keypoints[9].score
-        console.log("score leftwrist = " + scoreLeftWrist)
-        leftWristX=result[0].pose.leftWrist.x
-        LeftWristY=result[0].pose.leftWrist.y
+        scoreRightWrist=results[0].pose.keypoints[10].score
+        console.log("score leftwrist = " + scoreLeftWrist + "scoreRightWrist= " + scoreRightWrist)
+        leftWristX=results[0].pose.leftWrist.x
+        LeftWristY=results[0].pose.leftWrist.y
         console.log("LeftWristX= " + leftWristX + "LeftWristY= " + LeftWristY)
-        RightWristX=result[0].pose.rightWrist.x
-        RightWristY=result[0].pose.rightWrist.y
+        RightWristX=results[0].pose.rightWrist.x
+        RightWristY=results[0].pose.rightWrist.y
         console.log("RightWristX= " + RightWristX + "RightWristY= " + RightWristY)
     }
 }
@@ -39,21 +45,31 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,600,500)
-    Fill("#FF0000")
-    stroke("#FF0000")
+    song1_status=song1.isPlaying()
+    song2_status=song2.isPlaying()
+    fill("#0000FF")
+    stroke("#0000FF")
     if(scoreLeftWrist>0.2){
     circle(leftWristX,LeftWristY,20)
-    ny=Number(LeftWristY)
-    removeDecimals=floor(ny)
-    volume=removeDecimals/500
-    document.getElementById("volume").innerHTML="volume = " + volume
-    song.setVolume(volume)
+    song1.stop()
+    if(song2_status==false){
+        song2.play()
+        document.getElementById("name").innerHTML="playing avengers theme song"
     }
+    }
+    if(scoreRightWrist>0.2){
+        circle(RightWristX,RightWristY,20)
+        song2.stop()
+        if(song1_status==false){
+            song1.play()
+            document.getElementById("name").innerHTML="playing pokemon theme song"
+        }
+        }
 }
-
 function play(){
-    song.play()
-    song.setVolume(1)
-    song.rate(1)
+    song1.play()
+    song1.setVolume(1)
+    song1.rate(1)
+    
 }
 
